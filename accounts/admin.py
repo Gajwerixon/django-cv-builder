147ef1c3b7f10caee3665975d8d2b01487fd3c45
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-07-18 21:03:50 +0200

    feat: fix admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Admin configuration for the username-free CustomUser model."""
+
+    # Sort users alphabetically by email in the admin list view
+    ordering = ("email",)
+    
+    # Sort users alphabetically by email in the admin list view
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
+
+    # Layout structure for the user detail/edit page split into sections
+    fieldsets = (
+        (None, {
+            "fields": (
+                "email",
+                "password",
+            )
+        }),
+        ("Personal info", {
+            "fields": (
+                "first_name",
+                "last_name",
+            )
+        }),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+    )
