7d5656c489e5e848fb54e9621a3c3d4bb2b3c55c
Gajwerixon <217023425+Gajwerixon@users.noreply.github.com>
2026-09-03 13:34:18 +0200

    feat: improve trusted by section

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,17 @@ const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", () => {
     navbar.classList.toggle("scrolled", window.scrollY > 10);
 });
+
+const track = document.querySelector(".trusted-by-track");
+const group = document.querySelector(".trusted-by-group");
+
+if (track && group) {
+    const updateAnimation = () => {
+        const width = group.offsetWidth;
+
+        track.style.setProperty("--scroll-width", `${width}px`);
+    };
+
+    updateAnimation();
+    window.addEventListener("resize", updateAnimation);
+}
