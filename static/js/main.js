da79169d53ab61ed32d41fa1ac8dc32de25a1e87
Gajwerixon <217023425+Gajwerixon@users.noreply.github.com>
2026-09-12 12:38:02 +0200

    feat: carousel template-gallery

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,74 @@
+// Navbar
+
 const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
     navbar.classList.toggle("scrolled", window.scrollY > 10);
 });
 
-const track = document.querySelector(".trusted-by-track");
-const group = document.querySelector(".trusted-by-group");
 
-if (track && group) {
+// Testemonios
+
+const trustedTrack = document.querySelector(".trusted-by-track");
+const trustedGroup = document.querySelector(".trusted-by-group");
+
+if (trustedTrack && trustedGroup) {
     const updateAnimation = () => {
-        const width = group.offsetWidth;
+        const width = trustedGroup.offsetWidth;
 
-        track.style.setProperty("--scroll-width", `${width}px`);
+        trustedTrack.style.setProperty("--scroll-width", `${width}px`);
     };
 
     updateAnimation();
     window.addEventListener("resize", updateAnimation);
 }
+
+
+// Template Gallery
+
+const galleryTrack = document.querySelector(".gallery-track");
+const galleryPrevBtn = document.querySelector(".gallery-prev");
+const galleryNextBtn = document.querySelector(".gallery-next");
+const galleryCards = document.querySelectorAll(".cv");
+
+let galleryTrackPos = -145;
+let currentGalleryIdx = 2;
+
+galleryPrevBtn.addEventListener("click", () => {
+    
+    if (currentGalleryIdx > 0) {
+
+        currentGalleryIdx -= 1;
+        galleryTrackPos += 270;
+
+        galleryTrack.style.transform = `translateX(${galleryTrackPos}px)`;
+    }
+
+});
+
+galleryNextBtn.addEventListener("click", () => {
+
+    if (currentGalleryIdx < 4) {
+
+        currentGalleryIdx += 1;
+        galleryTrackPos -= 270;
+
+        galleryTrack.style.transform = `translateX(${galleryTrackPos}px)`;
+    }
+
+});
+
+galleryCards.forEach((card, index) => {
+    
+    card.addEventListener("click", () => {
+
+        currentGalleryIdx = index;
+
+        galleryTrackPos = -145 - (index - 2) * 270;
+
+        galleryTrack.style.transform = `translateX(${galleryTrackPos}px)`;
+
+
+    });
+
+});
